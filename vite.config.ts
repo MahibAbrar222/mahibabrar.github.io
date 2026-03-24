bf43c3fce250c4a52b57f4e45557c619979d0193
Mahib Abrar <mahib222@outlook.com>
2026-03-24 16:31:58 +0600

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from '@prerenderer/rollup-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), prerender({
-    routes: ['/', '/blog'],
-    renderer: '@prerenderer/renderer-puppeteer',
-    rendererOptions: {
-      renderAfterElementExists: '#root'
-    },
-  })],
-  base: '/'
+  plugins: [react(), 
+  //   prerender({
+  //   routes: ['/', '/blog'],
+  //   renderer: '@prerenderer/renderer-puppeteer',
+  //   rendererOptions: {
+  //     renderAfterDocumentEvent: 'render-event',
+  //     renderAfterTime: 2000, // Wait for 2 seconds before capturing the page
+  //   },
+  // })
+],
+  base: '/',
+  build: {
+    outDir: 'build',
+    target: 'es2019'
+  }
 })
